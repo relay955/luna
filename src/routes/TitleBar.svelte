b0d1f84cc3a948b24acdeaf0f7dd8f5e645d8c71
yhkim <yh.kim@ncodi.co.kr>
2024-06-19 10:59:54 +0900

    타이틀바 추가

---
+++ src/routes/TitleBar.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import { appWindow } from "@tauri-apps/api/window";
+  import Space from "./Space.svelte";
+  import logo from "../libs/icon.png";
+  const onClickMinimize = () => appWindow.minimize();
+  const onClickMaximize = () => appWindow.toggleMaximize();
+  const onClickClose = () => appWindow.close();
+</script>
+
+<div data-tauri-drag-region class="titlebar">
+  <img class="logo" src={logo} alt="luna_logo" />
+  <div class="title">Luna</div>
+  <Space />
+  <button
+    class="titlebar-button"
+    id="titlebar-minimize"
+    on:click={onClickMinimize}
+  >
+    <img
+      src="https://api.iconify.design/mdi:window-minimize.svg"
+      alt="minimize"
+    />
+  </button>
+  <button
+    class="titlebar-button"
+    id="titlebar-maximize"
+    on:click={onClickMaximize}
+  >
+    <img
+      src="https://api.iconify.design/mdi:window-maximize.svg"
+      alt="maximize"
+    />
+  </button>
+  <button class="titlebar-button" id="titlebar-close" on:click={onClickClose}>
+    <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+  </button>
+</div>
+
+<style lang="scss">
+  .logo{
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+  }
+  .title {
+    font-size: 14px;
+    font-weight: bold;
+    color: white;
+  }
+  .titlebar {
+    height: 30px;
+    background: #669ed5;
+    user-select: none;
+    display: flex;
+    justify-content: flex-end;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    align-items: center;
+    padding-left: 10px;
+    box-shadow: #c2c2c2 0px 0px 10px;
+  }
+  .titlebar-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    height: 30px;
+    border: none;
+    background: #669ed5;
+    img{
+      filter: invert(1);
+    }
+  }
+  .titlebar-button:hover {
+    background: #669ed5;
+  }
+</style>
