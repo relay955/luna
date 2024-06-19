896612ef3c6d89e7652588c52237c05a29871476
relay955 <refcell22@naver.com>
2024-06-19 10:59:54 +0900

    타이틀바 추가

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,25 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::Manager;
+use tauri::ShellScopeAllowedArg::Var;
+use window_shadows::set_shadow;
 
-fn main() {
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
+
+fn main() { 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(any(windows, target_os = "macos"))]
+            set_shadow(&app.get_window("main").unwrap(),true).unwrap();
+            
+            Ok(())
+        })
+        // .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
 }
